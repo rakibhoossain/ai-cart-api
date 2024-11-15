a68aa872c6bce5e34d9026f87930876ac732979e
Rakib Hossain <rakibhoossain@gmail.com>
2024-11-16 02:01:33 +0600

    products route

--- src/main/java/store/aicart/product/ProductResource.java
+++ src/main/java/store/aicart/product/ProductResource.java
@@ -1,7 +1,11 @@
 package store.aicart.product;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
 
 @Path("/product")
 public class ProductResource {
@@ -9,5 +13,13 @@ public class ProductResource {
     @GET
     public Long index() {
         return Product.count();
+    }
+
+
+    @GET
+    @Path("/products")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public List<Product> products() {
+        return Product.listAll();
     }
 }
